28c235c2c57306b5d0e4ecf4839c7f4860ceff7c
Albert Demeter <demeter@team23.de>
2024-02-22 11:26:37 +0100

    feat: simple component

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,17 +1,19 @@
-import type { StorybookConfig } from "@storybook/angular";
+import type { StorybookConfig } from '@storybook/angular';
+
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
-  framework: {
-    name: "@storybook/angular",
-    options: {},
-  },
-  docs: {
-    autodocs: "tag",
-  },
+    stories: ['../src/app/components/**/*.stories.ts'],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-interactions',
+        '@storybook/addon-a11y',
+    ],
+    framework: {
+        name: '@storybook/angular',
+        options: {},
+    },
+    docs: {
+        autodocs: 'tag',
+    },
 };
 export default config;
---
+++ src/app/components/task.component.ts
@@ -0,0 +1,41 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Task } from '../models/task.model';
+import { NgIf } from '@angular/common';
+
+@Component({
+    selector: 'app-task',
+    standalone: true,
+    templateUrl: `./task.component.html`,
+    imports: [
+        NgIf,
+    ],
+})
+export default class TaskComponent {
+    /**
+     * The shape of the task object
+     */
+    @Input() task?: Task;
+
+    @Output()
+    onPinTask = new EventEmitter<string>();
+
+    @Output()
+    onArchiveTask = new EventEmitter<string>();
+
+    /**
+     * @ignore
+     * Component method to trigger the onPin event
+     * @param id string
+     */
+    onPin(id: string) {
+        this.onPinTask.emit(id);
+    }
+    /**
+     * @ignore
+     * Component method to trigger the onArchive event
+     * @param id string
+     */
+    onArchive(id: string) {
+        this.onArchiveTask.emit(id);
+    }
+}
